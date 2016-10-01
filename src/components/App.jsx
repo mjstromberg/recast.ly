f1884f672003850f297724150bb64178180ea1bf
mjstromberg <michaeljstromberg@gmail.com>
2016-09-30 20:16:14 -0700

    Complete dynamic video list, dynamic video player, hook, youTube API, reusable API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,33 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoList: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  clickHandler (index) {
+    this.setState({
+      currentVideo: this.state.videoList[index]
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo} appState={this.state}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData} appState={this.state} clickHandler={this.clickHandler.bind(this)}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      q: options.query,
+      maxResults: options.max || 5,
+      key: options.key,
+      videoEmbeddable: true,
+      type: 'video',
+      part: 'snippet'
+    },
+    contentType: 'application/json',
+    success: function(data) {
+      console.log(data);
+      callback(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
